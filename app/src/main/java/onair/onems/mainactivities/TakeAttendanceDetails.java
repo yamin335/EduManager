ba6624bbe0a477e21c1f09fcc45b1bef368e455c
Yamin <myamin335@gmail.com>
2017-12-20 15:18:06 +0600

    Spinner modified...

---
+++ app/src/main/java/onair/onems/mainactivities/TakeAttendanceDetails.java
@@ -0,0 +1,16 @@
+package onair.onems.mainactivities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 12/20/2017.
+ */
+
+public class TakeAttendanceDetails extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
